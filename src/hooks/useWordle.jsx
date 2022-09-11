651d7edf3dbcb7cb28d8a0fc3840c9fa2e1c158e
AKSHAT-SINGH-DEV <AKSHATSINGHDELHI@GMAIL.COM>
2022-09-11 17:27:39 +0530

    Submitting Guesses

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -8,7 +8,7 @@ const useWordle = (solution) => {
     const [isCorrect, setIsCorrect] = useState(false);
 
     const formatGuess = () => {
-
+        console.log('formatting the guess - ', currentGuess);
     }
 
     const addNewGuess = () => {
@@ -16,6 +16,27 @@ const useWordle = (solution) => {
     }
 
     const handleKeyup = ({ key }) => {
+
+        if(key === 'Enter') {
+            // only add guess if turn is less than 5
+            if (turn > 5) {
+                console.log('you used all your guesses');
+                return;
+            }
+
+            // do not allow duplicate words
+            if (history.includes(currentGuess)) {
+                console.log('you already tried that word');
+                return;
+            }
+
+            // check word is 5 chars long
+            if (currentGuess.length !== 5) {
+                console.log('word must be 5 chars long');
+                return
+            }
+            formatGuess();
+        }
 
         if (key === 'Backspace') {
             setCurrentGuess((prev) => {
